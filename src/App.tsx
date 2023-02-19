2597fc4b26e52b44aba116adb69a7025834dc24d
eladjmc <eladjmc88@gmail.com>
2023-02-19 22:18:10 +0200

    initialize the project, added some modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Button from '@material-ui/core/Button';
+import CustomizedSteppers from './components/shared/Stepper';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      <Button variant="contained" color="secondary">Button</Button>
+      {/* <CustomizedSteppers/> */}
     </div>
   );
 }
