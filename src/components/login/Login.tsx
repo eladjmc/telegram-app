11a7ac3bf5d78c7b9ab5440ca1e874a4a38f9aed
eladjmc <eladjmc88@gmail.com>
2023-03-03 18:53:40 +0200

    made the single form component generic

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,15 +1,37 @@
-import React, { FormEvent } from "react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
+import { InputProps } from "../shared/ui-components/Input";
+import SingleFrom from "../shared/ui-components/SingleFrom";
 import UserForm from "../shared/ui-components/UserForm";
 const Login = () => {
-  const onSubmitForm = (e: FormEvent<HTMLInputElement>) => {
+  const [form,setForm] = useState({
+    email:'',
+    password: '',
+  });
+  const onChange = (e:ChangeEvent<HTMLInputElement>,label:string) => {
+    setForm({...form,[label.toLowerCase()]:e.target.value});
+  }
+  const onSubmitForm = () => {
     console.log("submitted");
     
   };
-  
+  const inputs:InputProps[] = [
+    {
+      label:'Email',
+      type:'email',
+      value:form.email,
+      placeholder:' ',
+      onChange,
+    },
+    {
+      label:'Password',
+      type:'password',
+      value:form.password,
+      placeholder:' ',
+      onChange,
+    },
+  ]
   return (
-    <UserForm fields={["Email", "Password"]} submitForm={onSubmitForm}>
-      <h3>Login</h3>
-    </UserForm>
+    <SingleFrom inputs={inputs} title={'Login'} onSubmit={onSubmitForm} />
   );
 };
 
--- src/pages/PhonesPage.tsx
+++ src/pages/PhonesPage.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Phones = () => {
   return (
-    <div>Phones</div>
+    <section>Phones</section>
   )
 }
 
