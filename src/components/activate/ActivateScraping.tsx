0c9cf7519fc4ba27b1aec319761d7380b673a0d0
eladjmc <eladjmc88@gmail.com>
2023-03-11 05:23:20 +0200

    added a stop scrapinf button while scraping is activated

--- src/components/activate/ActivateScraping.tsx
+++ src/components/activate/ActivateScraping.tsx
@@ -3,23 +3,49 @@ import "./ActivateScraping.scss";
 
 const ActivateScraping = () => {
   const [isScraping, setIsScraping] = useState(false);
+
   const handleScraping = async () => {
-    setIsScraping(true);
+    try {
+      setIsScraping(true);
+    } catch (error) {}
   };
+
+  const handleStopScraping = () => {
+    setIsScraping(false);
+  };
+
   return (
-    <div onClick={handleScraping} className="button-container">
+    <div className="button-container">
       {!isScraping ? (
-        <button className="button-82-pushable">
+        <button onClick={handleScraping} className="button-82-pushable">
           <span className="button-82-shadow"></span>
           <span className="button-82-edge"></span>
           <span className="button-82-front text">Activate Scraping</span>
           {isScraping && <div className="lds-hourglass"></div>}
         </button>
       ) : (
+        <>
           <button disabled={isScraping} className="button-82-pushable">
-          <span className="button-82-front-disabled text">Scraping</span>
-          {isScraping && <div className="lds-hourglass"></div>}
-        </button>
+            <span className="button-82-shadow"></span>
+            <span className="button-82-edge"></span>
+            <span className="button-82-front-disabled text">Scraping</span>
+            {isScraping && <div className="lds-hourglass"></div>}
+          </button>
+
+          <button onClick={() => handleStopScraping()} className="noselect">
+            <span className="text-end-btn">Stop</span>
+            <span className="icon">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path>
+              </svg>
+            </span>
+          </button>
+        </>
       )}
     </div>
   );
